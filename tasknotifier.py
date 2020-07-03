1def49453920d0c1070ded7c31ffad2af4657061
shubhamsoni897 <52289166+shubhamsoni897@users.noreply.github.com>
2020-07-03 20:56:16 +0530

    Update tasknotifier.py

--- tasknotifier.py
+++ tasknotifier.py
@@ -3,8 +3,8 @@ from plyer import notification
 while True:
     notification.notify(
     title="Please Drink Water Now",
-    message="You drink atleast 10 litres of water a day",
-    timeout=2
+     message="You drink atleast 10 litres of water a day", #Your message here
+    timeout=2  #timeout for the display
     )
-    time.sleep(6)
+    time.sleep(6) #sleep time
 
